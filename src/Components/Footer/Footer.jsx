f5dcc45719b8576cbbe5f39ac188447a5fafe2b4
rahmanShuhin <suhin4000@gmail.com>
2021-08-24 21:35:13 +0600

    footer updated

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,7 +1,20 @@
 import React from "react";
 
 const Footer = () => {
-    return <div></div>;
+    return (
+        <footer className="footer">
+            <div className="footer__left">
+                <h2>Help Make Knowledge Beautiful Again</h2>
+                <p>
+                    The Rosetta Foundation is a non-profit on a mission to make
+                    knowledge a beautiful global public good once again. It’s
+                    100% open-source to build on top of. Built, owned, and
+                    operated by a community of open source developers like
+                    yourself.{" "}
+                </p>
+            </div>
+        </footer>
+    );
 };
 
 export default Footer;
