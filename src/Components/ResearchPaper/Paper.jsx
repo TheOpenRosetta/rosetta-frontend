47f1bf0ec4ab01aa3773d8ffbc840e227417e33d
rahmanShuhin <suhin4000@gmail.com>
2021-08-26 10:48:36 +0600

    paper added

--- src/Components/ResearchPaper/Paper.jsx
+++ src/Components/ResearchPaper/Paper.jsx
@@ -1,19 +1,11 @@
 import React from "react";
-import paper from "../../images/paper.png";
+import paper from "../../images/paper_group.png";
 const Paper = () => {
     return (
         <section className="paper">
-            {/* <div className="container">
-                <img src={paper} alt="Research Paper" />
-                <img src={paper} alt="Research Paper" />
-                <img src={paper} alt="Research Paper" />
-                <div>
-                    <img src={paper} alt="Research Paper" />
-                    <img src={paper} alt="Research Paper" />
-                </div>
-                <img src={paper} alt="Research Paper" />
-                <img src={paper} alt="Research Paper" />
-            </div> */}
+            <div className="container">
+                <img src={paper} alt="" />
+            </div>
         </section>
     );
 };
