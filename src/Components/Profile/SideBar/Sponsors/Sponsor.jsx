a045d68195130f0a0f0e9ef78ee6e4b0824bf6ab
shahriad00 <shahriad555@gmail.com>
2021-08-27 01:15:06 +0600

    profile added

---
+++ src/Components/Profile/SideBar/Sponsors/Sponsor.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const Sponsor = (props) => {
+    return (
+        <div className="sponsors">
+            <h3>{props.name}</h3>
+            <div className="content__wrapper">
+                <div className="content"/>
+                <div className="content"/>
+                <div className="content"/>
+                <div className="content"/>
+                <div className="content"/>
+                <div className="content"/>
+            </div>
+            
+        </div>
+    )
+}
+
+export default Sponsor;
