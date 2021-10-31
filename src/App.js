89e83353ded22160a8c486b683388c2fb7bac960
simianware <simianware@gmail.com>
2021-11-01 09:53:51 +1300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import "./styles/App.css";
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Homepage from "./pages/Homepage";
 import Profile from "./pages/Profile";
 import { useEffect } from "react";
