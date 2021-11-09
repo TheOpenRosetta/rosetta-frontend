6bd13556235290eacbc698593255e316a349a95e
simianware <simianware@gmail.com>
2021-11-10 00:48:39 +1300

    Update Pagination.jsx

--- src/Components/Pagination/Pagination.jsx
+++ src/Components/Pagination/Pagination.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Link } from "react-router-dom";
+// import { Link } from "react-router-dom";
 import PaginationMu from "@mui/material/Pagination";
 
 const Pagination = ({
