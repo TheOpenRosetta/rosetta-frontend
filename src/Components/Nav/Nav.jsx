9ecc66e7b6d1b1d766bbda1b78c731bbedd15113
Roman Shprenger <shprengerroman@gmail.com>
2021-12-14 17:17:53 +0300

    Fix header menu on mobile

--- src/Components/Nav/Nav.jsx
+++ src/Components/Nav/Nav.jsx
@@ -1,52 +1,49 @@
-import React, { useRef } from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import logo from "../../images/ROSETTA.svg";
 import search from "../../images/search-normal.svg";
 const Nav = ({ show }) => {
-    const menu = useRef(null);
-    const handleHamburger = () => {
-        document.getElementById("button1").classList.toggle("active");
-    };
+    const [burger, setBurger] = useState(false);
     return (
         <nav className="nav">
             <div className="container">
                 <Link to="/">
                     <img src={logo} alt="ROSETTA LOGO" />
                 </Link>
-                {show && (
-                    <div className="nav__search menu__for__desktop">
-                        <form>
-                            <img src={search} alt="" />
-                            <input
-                                type="text"
-                                placeholder="Search for authors |"
-                            />
-                            <button type="submit">Search</button>
-                        </form>
-                        <div>
-                            <Link>Sign In</Link>
-                        </div>
-                    </div>
-                )}
-                <div className="menu__for__desktop">
-                    {show ? (
-                        <button>Create Free Account</button>
-                    ) : (
-                        <>
-                            <button>
-                                <a href="https://rosetta-2.gitbook.io/rosetta-docs/">
-                                    Docs
-                                </a>
-                            </button>
-                            <button>Sign In</button>
-                        </>
-                    )}
+                <div className={`nav-menu ${burger ? 'active' : ''}`}>
+                  {show && (
+                      <div className="nav__search menu__for__desktop">
+                          <form>
+                              <img src={search} alt="search icon" />
+                              <input
+                                  type="text"
+                                  placeholder="Search for authors |"
+                              />
+                              <button type="submit">Search</button>
+                          </form>
+                          <div>
+                              <Link>Sign In</Link>
+                          </div>
+                      </div>
+                  )}
+                  <div className="menu__for__desktop">
+                      {show ? (
+                          <button>Create Free Account</button>
+                      ) : (
+                          <>
+                              <button>
+                                  <a href="https://rosetta-2.gitbook.io/rosetta-docs/">
+                                      Docs
+                                  </a>
+                              </button>
+                              <button>Sign In</button>
+                          </>
+                      )}
+                  </div>
                 </div>
                 <div
-                    className="menu"
-                    id="button1"
-                    ref={menu}
-                    onClick={handleHamburger}
+                    className={`menu ${burger ? 'active' : ''}`}
+                    onClick={() => setBurger(!burger)}
                 >
                     <span></span>
                     <span></span>
