df60f22ac66440a2ac1a16e80024e03ceef59155
Roman Shprenger <shprengerroman@gmail.com>
2021-12-14 16:50:41 +0300

    Changed landing page

--- src/Components/Landing/Landing.jsx
+++ src/Components/Landing/Landing.jsx
@@ -1,164 +1,88 @@
 import React from "react";
-
-// import search from "../../images/search-normal.svg";
-// import { css } from "@emotion/react";
-// import PuffLoader from "react-spinners/PuffLoader";
 import Particle from "../Particle/Particle";
-// import { DataContext } from "../../StateManagement/AuthorsContext";
 import img1 from "../../images/open access.png";
 import img2 from "../../images/open source.png";
 import img3 from "../../images/non profit.png";
 import { useHistory } from "react-router-dom";
+
 const Landing = () => {
     let history = useHistory();
-    // const [result, setResult] = useState({
-    //     data: [],
-    //     loading: false,
-    // });
-    // const override = css`
-    //     display: block;
-    //     margin: 0 auto;
-    //     border-color: red;
-    // `;
-    // const [name, setName] = useState("");
-    // const [authors, setAuthors] = useContext(DataContext);
-
-    // const handleForm = (e) => {
-    //     const temp = { ...result };
-    //     temp.loading = true;
-    //     setResult(temp);
-    //     if (name) {
-    //         fetch(
-    //             `https://rosetta.eastus.cloudapp.azure.com/api/getauthors/${name}`
-    //         )
-    //             .then((response) => response.json())
-    //             .then((Data) =>
-    //                 setResult({
-    //                     data: Data,
-    //                     loading: false,
-    //                 })
-    //             );
-    //     } else {
-    //         setResult({
-    //             data: [],
-    //             loading: false,
-    //         });
-    //     }
-    //     e.preventDefault();
-    // };
-    // const handleChange = (e) => {
-    //     if (e.target.value) {
-    //         setName(e.target.value);
-    //     } else {
-    //         setResult({
-    //             data: [],
-    //             loading: false,
-    //         });
-    //     }
-    // };
-    // useEffect(() => {
-    //     setAuthors(result.data);
-    // }, [result]);
     return (
-        <section className="hero">
-            <Particle></Particle>
+        <>
+          <section className="hero">
+              <Particle></Particle>
+              <div className="container">
+                  <p>Introducing Rosetta:</p>
+                  <h1>
+                      The new scientist owned collaborative network with open markets for knowledge for any scientist to own, and earn.
+                  </h1>
+                  <p>
+                      Now the average paper published earns the author $1,000 a year in ownership rewards, and gets archived permenantly on the most powerful persistent repository to ensure your knowledge can never be destroyed.
+                  </p>
+                  <div className="two__btn">
+                      <button className="btn btn__primary" onClick={() => history.push("/start/")}>
+                          Get Early Access
+                      </button>
+                  </div>
+                  <div className="three__img">
+                      <img src={img1} alt="illustration 1" />
+                      <img src={img3} alt="illustration 2" />
+                      <img src={img2} alt="illustration 3" />
+                  </div>
+              </div>
+              <div className="graph__stats">
+                  <div className="graph__stats__body">
+                      <span>
+                          {" "}
+                          Our
+                          <br /> Knowledge
+                          <br /> Graph Stats{" "}
+                      </span>
+                      <br />
+                      <div>
+                          <h5> 271m+ </h5>
+                          <span> Authors </span>
+                      </div>
+                      <div>
+                          <h5> 136m+ </h5>
+                          <span> Papers </span>
+                      </div>
+                      <div>
+                          <h5> 1.8bn+ </h5>
+                          <span> Citations </span>
+                      </div>
+                  </div>
+              </div>
+          </section>
+          <section className="landing__description">
             <div className="container">
-                <p>Introducing Rosetta</p>
-                <h1>
-                    The new peer-to-peer academic social collaborative network owned by authors
-                </h1>
-                <p>
-                    Rosetta is the most powerful social platform to share your knowledge, and earn just by publishing open access replicable quality papers. The average paper published earns the author $1,000 a year in ownership rewards on our network based on its ImpactScore, and gets archived permenantly on the most powerful distributed network to ensure your knowledge can never be destroyed.
-                </p>
-                {/* <div className="search__box">
-                    <form onSubmit={handleForm}>
-                        <img src={search} alt="" />
-                        <input
-                            type="text"
-                            onChange={(e) => handleChange(e)}
-                            placeholder="Search for researcher and papers "
-                        />
-                        {!result.loading && (
-                            <button type="submit" disabled={result.loading}>
-                                Search
-                            </button>
-                        )}
-                        <PuffLoader
-                            color={"#b68e5c"}
-                            loading={result.loading}
-                            css={override}
-                            size={40}
-                        />
-                    </form>
-                    {result.data.length !== 0 && (
-                        <div className="search__result">
-                            {result.data.map((x, index) => (
-                                <Link
-                                    to={`/profile/${x.authorId}/${x.normalizedName}/`}
-                                    key={index}
-                                >
-                                    <div>
-                                        <div></div>
-                                        <div>
-                                            <p>{x.displayName}</p>
-                                            <small>
-                                                Total Paper: {x.paperCount}
-                                            </small>
-                                        </div>
-                                        <Link
-                                            to={`/profile/${x.authorId}/${x.normalizedName}/`}
-                                        >
-                                            Profile
-                                        </Link>
-                                    </div>
-                                </Link>
-                            ))}
-                        </div>
-                    )}
-                </div> */}
-                <div className="two__btn">
-                    <button onClick={() => history.push("/start/")}>
-                        Get Early Access
-                    </button>
-                    <button onClick={() => history.push("/features/")}>
-                        Learn more
-                    </button>
+              <div className="description">
+                <div className="description__title">The most powerful social platform for discovery, and distribution.</div>
+                <div className="description__text">
+                  <p>Now you can earn by publishing open access replicable quality papers for free.</p>
+                  <p>When you publish on Rosetta’s open access permissionless persistent repository, you create a market for your paper that you own, and earn the rewards based on it’s Impact.</p>
                 </div>
-                <p>
-                    Over <span>271+ Million </span> authors can now see how much
-                    ownership, and extra income you can claim from your past
-                    papers in under 2 minutes.
-                </p>
-                <div className="three__img">
-                    <img src={img1} alt="" />
-                    <img src={img3} alt="" />
-                    <img src={img2} alt="" />
+                <div className="description__btn">
+                  <button className="btn" onClick={() => history.push("/features/")}>
+                      Learn more
+                  </button>
                 </div>
+              </div>
             </div>
-            <div className="graph__stats">
-                <div className="graph__stats__body">
-                    <span>
-                        {" "}
-                        Our
-                        <br /> Knowledge
-                        <br /> Graph Stats{" "}
-                    </span>
-                    <br />
-                    <div>
-                        <h5> 271m+ </h5>
-                        <span> Authors </span>
-                    </div>
-                    <div>
-                        <h5> 136m+ </h5>
-                        <span> Papers </span>
-                    </div>
-                    <div>
-                        <h5> 1.8bn+ </h5>
-                        <span> Citations </span>
-                    </div>
-                </div>
+          </section>
+          <section className="landing__ownership">
+            <div className="container">
+              <div className="ownership__text">
+                Over <span>271+ Million</span> authors can now see how much ownership, and extra income you can claim from your past papers in under 2 minutes.
+              </div>
+              <div className="ownership__logos">
+                <img src={img1} alt="illustration 1" />
+                <img src={img3} alt="illustration 2" />
+                <img src={img2} alt="illustration 3" />
+              </div>
             </div>
-        </section>
+          </section>
+        </>
     );
 };
 
